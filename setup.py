344ab63074c3a425d0107ad17d0ca1c27104c3fa
David Sanchez <david.sanchez.falero@gmail.com>
2018-06-21 12:00:50 +0200

    setup update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='django-requests-cache',
-    version='0.1.0',
+    version='0.1.3',
     description='Django Cache.',
     long_description=long_description,
     url='https://github.com/davidsanfal/django-requests-cache',
